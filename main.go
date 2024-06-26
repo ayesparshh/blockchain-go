84c1edec3f5e24a28a8168c67024393d1461c03a
Ssparshgg <wasteid872@gmail.com>
2024-06-26 14:01:18 +0530

    start

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+func main () {
+	r := mux.NewRouter()
+	r.HandleFunc("/", getBlockchain).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
+	r.HandleFunc("/new", newMine).Methods("POST")
+	
+	log.Println("listening on port 3000")
+
+	log.Fatal(http.ListenAndServe(":3000",r))
+}
